426b32aa08080a019490d290f4908b5218d25394
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-17 11:03:21 -0600

    Add test for customized params (both)

--- spec/models/application_record_spec.rb
+++ spec/models/application_record_spec.rb
@@ -15,9 +15,18 @@ RSpec.describe ApplicationRecord do
         expect(items.paginate.last).to eq items[19]
       end
 
-      # it 'returns a list of objects based on input of given pagination params (both)' do
+      it 'returns a list of objects based on input of given pagination params (both)' do
+        create(:merchant_with_items, items_count: 30)
 
-      # end
+        merchant = Merchant.first
+        items = merchant.items
+
+        paginated = items.paginate({page: 3, per_page: 10})
+        expect(paginated).to be_an Array
+        expect(paginated.count).to eq 10
+        expect(paginated.first).to eq items[20]
+        expect(paginated.last).to eq items[29]
+      end
     end
   end
 end
