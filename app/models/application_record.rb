c1361eb45ccc8367a14f6bf263af1b0313499dc0
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-17 11:09:06 -0600

    Update tests and pagination method for flexible inputs

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,14 +1,14 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def self.paginate(settings = {page: 1, per_page: 20})
-    # settings = defaults.merge(settings)
+  def self.paginate(settings = {})
+    settings = defaults.merge(settings)
     all.limit(settings[:per_page])
        .offset((settings[:page].to_i - 1) * settings[:per_page])
        .to_a
   end
 
-  # def self.defaults
-  #   {page: 1, per_page: 20}
-  # end
+  def self.defaults
+    {page: 1, per_page: 20}
+  end
 end
--- spec/models/application_record_spec.rb
+++ spec/models/application_record_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe ApplicationRecord do
         expect(items.paginate.last).to eq items[19]
       end
 
-      it 'returns a list of objects based on input of given pagination params (both)' do
+      it 'returns a list of objects given pagination params (both)' do
         create(:merchant_with_items, items_count: 30)
 
         merchant = Merchant.first
@@ -26,6 +26,32 @@ RSpec.describe ApplicationRecord do
         expect(paginated.count).to eq 10
         expect(paginated.first).to eq items[20]
         expect(paginated.last).to eq items[29]
+      end
+
+      it 'returns a list of objects given page num only (default 20 per page)' do
+        create(:merchant_with_items, items_count: 50)
+
+        merchant = Merchant.first
+        items = merchant.items
+
+        paginated = items.paginate({page: 2})
+        expect(paginated).to be_an Array
+        expect(paginated.count).to eq 20
+        expect(paginated.first).to eq items[20]
+        expect(paginated.last).to eq items[39]
+      end
+
+      it 'returns a list of objects given per_page only (default to first page)' do
+        create(:merchant_with_items, items_count: 30)
+
+        merchant = Merchant.first
+        items = merchant.items
+
+        paginated = items.paginate({per_page: 10})
+        expect(paginated).to be_an Array
+        expect(paginated.count).to eq 10
+        expect(paginated.first).to eq items.first
+        expect(paginated.last).to eq items[9]
       end
     end
   end
