0f95c203d76f6795e20eab87bd05eae40fc3d819
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-18 15:57:59 -0600

    Run rubocop and repair offenses

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,6 +2,7 @@ module ExceptionHandler
   extend ActiveSupport::Concern
 
   class MissingToken < StandardError; end
+
   class InvalidToken < StandardError; end
 
   included do
@@ -9,14 +10,14 @@ module ExceptionHandler
     rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
     rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
 
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
+    rescue_from ActiveRecord::RecordNotFound do |error|
+      json_response({ message: error.message }, :not_found)
     end
   end
 
   private
 
-  def four_twenty_two(e)
-    json_response({ message: e.message }, :unprocessable_entity)
+  def four_twenty_two(error)
+    json_response({ message: error.message }, :unprocessable_entity)
   end
 end
