72b69fb7e3d452d22a06427651f726bc24c4c9df
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-20 13:11:57 -0600

    Add class method for merchant search

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,7 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
+
+  def self.find_first_by_name(name)
+    find_by_sql("select * from merchants where name ilike '%#{name}%' order by lower(name) asc limit 1;").first
+  end
 end
