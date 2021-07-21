22864bc307b3bfc36bf364eee04bbc74bacd8e9d
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-20 21:47:31 -0600

    Add helper to validate merchant search params

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def find
-    return json_response({}, :bad_request) if params[:name].nil? or params[:name] == ''
+    return json_response({}, :bad_request) if !valid_search?(params)
     @merchant = Merchant.find_first_by_name(params[:name])
     return json_response({}) if @merchant.nil?
   
     formatted = format_merchant_json(@merchant)
     json_response(formatted)
+  end
+
+  private
+
+  def valid_search?(params)
+    params[:name].present?
   end
 end
