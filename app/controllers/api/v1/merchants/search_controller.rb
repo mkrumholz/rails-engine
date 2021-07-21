6e62f5c8d7afe49dd0876b4b1f01376c357882a0
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-21 10:18:20 -0600

    Complete transition from formatters to serializers

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def find
-    return json_response({}, :bad_request) unless valid_search?(params)
+    return json_response({data: {}}, :bad_request) unless valid_search?(params)
 
     @merchant = Merchant.find_first_by_name(params[:name])
-    return json_response({}) if @merchant.nil?
+    return json_response({data: {}}) if @merchant.nil?
 
     json_response(MerchantSerializer.new(@merchant))
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
   include ExceptionHandler
-  include MerchantFormatter
   include Paginator
   include Response
 end
