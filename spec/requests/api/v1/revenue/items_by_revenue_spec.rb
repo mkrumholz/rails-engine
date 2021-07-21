3ea20d1458ae63c37873dda109468b84410cf86d
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-21 16:45:45 -0600

    Build out model relationships for remaining DB tables

---
+++ spec/requests/api/v1/revenue/items_by_revenue_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Items by revenue' do
+  context 'no quantity is specified' do
+    it 'returns the top 10 items by revenue generated' do
+      create(:customer)
+      create_list(:merchant, 3) do |merchant| 
+        create_list(:item, 5, merchant: merchant) do |item|
+          invoice = create(:invoice, customer: customer, merchant: merchant)
+          create(:invoice_item, invoice: invoice, item: item)
+        end
+      end
+      
+
+    end
+  end
+
+  context 'quantity is specified and less than 1 or not an integer' do
+    it 'returns an error message and 400 status code' do
+      
+    end
+  end
+end
