f152a8a260ee2ef0ec86f9834b4bc900230b0050
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-23 18:03:19 -0600

    Refactor after presentation feedback

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,12 +7,11 @@ class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
 
   def self.revenue_for_range(start_date, end_date)
-    # end_date = (Date.parse(end_date) + 1).to_s
+    end_date = (Date.parse(end_date) + 1).to_s
     joins(:transactions, :invoice_items)
       .where(transactions: { result: 'success' })
       .where(invoices: { status: 'shipped' })
-      .where("invoices.created_at between ? and date(?) + interval '1 day'", start_date, end_date)
-      # .where("?::tsrange @> invoices.created_at", "[#{start_date}, #{end_date})")
+      .where("?::tsrange @> invoices.created_at", "[#{start_date}, #{end_date})")
       .sum('invoice_items.quantity * invoice_items.unit_price')
   end
 
--- spec/requests/api/v1/items/items_request_spec.rb
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -42,9 +42,7 @@ RSpec.describe "Items API Requests" do
     context 'when pagination params are present' do
       context 'and only per_page is specified' do
         it 'sends the appropriate list length and defaults to page 1' do
-          create(:merchant_with_items, items_count: 30)
-
-          all_items = Item.all
+          merchant = create(:merchant_with_items, items_count: 30)
 
           get '/api/v1/items', params: {per_page: 10}
 
@@ -53,39 +51,14 @@ RSpec.describe "Items API Requests" do
           items = JSON.parse(response.body, symbolize_names: true)
 
           expect(items[:data].count).to eq(10)
-          expect(items[:data].first[:id]).to eq all_items.first.id.to_s
-          expect(items[:data].last[:id]).to eq all_items[9].id.to_s
-
-          items[:data].each do |item|
-            expect(item).to have_key(:id)
-            expect(item[:id]).to be_an(String)
-
-            expect(item).to have_key(:type)
-            expect(item[:type]).to eq "item"
-
-            expect(item).to have_key(:attributes)
-            expect(item[:attributes]).to be_a Hash
-
-            expect(item[:attributes]).to have_key(:name)
-            expect(item[:attributes][:name]).to be_an(String)
-
-            expect(item[:attributes]).to have_key(:description)
-            expect(item[:attributes][:description]).to be_an(String)
-
-            expect(item[:attributes]).to have_key(:unit_price)
-            expect(item[:attributes][:unit_price]).to be_an(Float)
-            
-            expect(item[:attributes]).to have_key(:merchant_id)
-            expect(item[:attributes][:merchant_id]).to be_an(Integer)
-          end
+          expect(items[:data].first[:id]).to eq merchant.items.first.id.to_s
+          expect(items[:data].last[:id]).to eq merchant.items[9].id.to_s
         end
       end
 
       context 'and only page is specified' do
         it 'sends the correct items for page and defaults to 20 per page' do
-          create(:merchant_with_items, items_count: 30)
-
-          all_items = Item.all
+          merchant = create(:merchant_with_items, items_count: 30)
 
           get '/api/v1/items', params: {page: 2}
 
@@ -94,37 +67,12 @@ RSpec.describe "Items API Requests" do
           items = JSON.parse(response.body, symbolize_names: true)
 
           expect(items[:data].count).to eq(10)
-          expect(items[:data].first[:id]).to eq all_items[20].id.to_s
-          expect(items[:data].last[:id]).to eq all_items.last.id.to_s
-
-          items[:data].each do |item|
-            expect(item).to have_key(:id)
-            expect(item[:id]).to be_an(String)
-
-            expect(item).to have_key(:type)
-            expect(item[:type]).to eq "item"
-
-            expect(item).to have_key(:attributes)
-            expect(item[:attributes]).to be_a Hash
-
-            expect(item[:attributes]).to have_key(:name)
-            expect(item[:attributes][:name]).to be_an(String)
-
-            expect(item[:attributes]).to have_key(:description)
-            expect(item[:attributes][:description]).to be_an(String)
-
-            expect(item[:attributes]).to have_key(:unit_price)
-            expect(item[:attributes][:unit_price]).to be_an(Float)
-            
-            expect(item[:attributes]).to have_key(:merchant_id)
-            expect(item[:attributes][:merchant_id]).to be_an(Integer)
-          end
+          expect(items[:data].first[:id]).to eq merchant.items[20].id.to_s
+          expect(items[:data].last[:id]).to eq merchant.items.last.id.to_s
         end
 
         it 'defaults to page 1 if page param is specified as < 1' do
-          create(:merchant_with_items, items_count: 30)
-
-          all_items = Item.all
+          merchant = create(:merchant_with_items, items_count: 30)
 
           get '/api/v1/items', params: {page: 0}
 
@@ -133,39 +81,14 @@ RSpec.describe "Items API Requests" do
           items = JSON.parse(response.body, symbolize_names: true)
 
           expect(items[:data].count).to eq(20)
-          expect(items[:data].first[:id]).to eq all_items.first.id.to_s
-          expect(items[:data].last[:id]).to eq all_items[19].id.to_s
-
-          items[:data].each do |item|
-            expect(item).to have_key(:id)
-            expect(item[:id]).to be_an(String)
-
-            expect(item).to have_key(:type)
-            expect(item[:type]).to eq "item"
-
-            expect(item).to have_key(:attributes)
-            expect(item[:attributes]).to be_a Hash
-
-            expect(item[:attributes]).to have_key(:name)
-            expect(item[:attributes][:name]).to be_an(String)
-
-            expect(item[:attributes]).to have_key(:description)
-            expect(item[:attributes][:description]).to be_an(String)
-
-            expect(item[:attributes]).to have_key(:unit_price)
-            expect(item[:attributes][:unit_price]).to be_an(Float)
-            
-            expect(item[:attributes]).to have_key(:merchant_id)
-            expect(item[:attributes][:merchant_id]).to be_an(Integer)
-          end
+          expect(items[:data].first[:id]).to eq merchant.items.first.id.to_s
+          expect(items[:data].last[:id]).to eq merchant.items[19].id.to_s
         end
       end
       
       context 'and both params are specified' do
         it 'sends the appropriate items and list length for the params' do
-          create(:merchant_with_items, items_count: 30)
-
-          all_items = Item.all
+          merchant = create(:merchant_with_items, items_count: 30)
 
           get '/api/v1/items', params: {page: 2, per_page: 10}
 
@@ -174,31 +97,8 @@ RSpec.describe "Items API Requests" do
           items = JSON.parse(response.body, symbolize_names: true)
 
           expect(items[:data].count).to eq(10)
-          expect(items[:data].first[:id]).to eq all_items[10].id.to_s
-          expect(items[:data].last[:id]).to eq all_items[19].id.to_s
-
-          items[:data].each do |item|
-            expect(item).to have_key(:id)
-            expect(item[:id]).to be_an(String)
-
-            expect(item).to have_key(:type)
-            expect(item[:type]).to eq "item"
-
-            expect(item).to have_key(:attributes)
-            expect(item[:attributes]).to be_a Hash
-
-            expect(item[:attributes]).to have_key(:name)
-            expect(item[:attributes][:name]).to be_an(String)
-
-            expect(item[:attributes]).to have_key(:description)
-            expect(item[:attributes][:description]).to be_an(String)
-
-            expect(item[:attributes]).to have_key(:unit_price)
-            expect(item[:attributes][:unit_price]).to be_an(Float)
-            
-            expect(item[:attributes]).to have_key(:merchant_id)
-            expect(item[:attributes][:merchant_id]).to be_an(Integer)
-          end
+          expect(items[:data].first[:id]).to eq merchant.items[10].id.to_s
+          expect(items[:data].last[:id]).to eq merchant.items[19].id.to_s
         end
       end
     end
